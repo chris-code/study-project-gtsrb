364ba27d7f2f93c821af5a79294ebf5f35410252
chris-code <chris-code@users.noreply.github.com>
2015-12-15 10:59:15 +0100

    Fixed AttributeError related to weight io.

--- src/GTSRB.py
+++ src/GTSRB.py
@@ -41,32 +41,22 @@ def build_model(input_shape):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('path', help='Path to csv file that lists input images')
-parser.add_argument('-r', '--resolution', help='Resample images to AxB resolution. Default is 48x48.')
-parser.add_argument('-d', '--datalimit', help='Maximum number of data points to read from PATH', type=int)
+parser.add_argument('-e', '--epochs', help='Numper of epochs to train for', type=int, default=10)
+parser.add_argument('-r', '--resolution', help='Resample images to AxB resolution. Default is 48x48.', default='48x48')
+parser.add_argument('-d', '--datalimit', help='Maximum number of data points to read from PATH', type=int, default=None)
 parser.add_argument('-l', '--load-weights', help='Load weights from specified file')
 parser.add_argument('-s', '--store-weights', help='Store weights to specified file')
-parser.add_argument('-v', '--verbose', help='Set the verbosity level of keras (valid values: 0, 1, 2)', type=int)
+parser.add_argument('-v', '--verbose', help='Set the verbosity level of keras (valid values: 0, 1, 2)', type=int, default=1)
 args = parser.parse_args()
-if args.resolution:
-	try:
-		sizes = args.resolution.split('x', maxsplit=1)
-		resolution = (int(sizes[0]), int(sizes[1]))
-	except:
-		print('Invalid resolution specification: \'{0}\''.format(args.resolution))
-		print('Resolution must be of the format \'AxB\'')
+try:
+	sizes = args.resolution.split('x', maxsplit=1)
+	resolution = (int(sizes[0]), int(sizes[1]))
+except:
+	print('Invalid resolution specification: \'{0}\''.format(args.resolution))
+	print('Resolution must be of the format \'AxB\'')
 	exit(1)
-else:
-	resolution = (48, 48)
-if args.datalimit:
-	data_limit = args.datalimit
-else:
-	data_limit = None
-if args.verbose:
-	verbosity = args.verbose
-else:
-	verbosity = 1
 
-x_train, y_train = GTSRB_io.read_data(args.path, resolution, data_limit)
+x_train, y_train = GTSRB_io.read_data(args.path, resolution, args.datalimit)
 class_count = int(np.max(y_train) + 1)
 if class_count != 43:
 	print('There are {0} classes instead of 43!'.format(class_count))
@@ -74,14 +64,17 @@ if class_count != 43:
 y_train = np_utils.to_categorical(y_train, class_count)
 
 input_shape = (x_train.shape[1], x_train.shape[2], x_train.shape[3])
-model = build_model(input_shape);
-if args.load-weights:
-	model.load_weights(args.load-weights)
+model = build_model(input_shape)
 #~ model.summary()
 
-model.fit(x_train, y_train, nb_epoch=10, show_accuracy=True, verbose=verbosity)
+if args.load_weights:
+	print('Loading weights from {0}'.format(args.load_weights))
+	model.load_weights(args.load_weights)
 
-if args.store-weights:
-	model.save_weights(args.store-weights)
+model.fit(x_train, y_train, nb_epoch=args.epochs, show_accuracy=True, verbose=args.verbose)
+
+if args.store_weights:
+	print('Storing weights to {0}'.format(args.store_weights))
+	model.save_weights(args.store_weights)
 
 
