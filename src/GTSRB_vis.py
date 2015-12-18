80309675f94510bce86834025260facc724e88e7
Yannick <Yannick.Schroer@rub.de>
2015-12-18 14:55:53 +0100

    Method to visualize the filters

--- src/GTSRB_vis.py
+++ src/GTSRB_vis.py
@@ -5,7 +5,7 @@ import PIL.Image as pil
 
 import GTSRB_nn
 
-def visualize_filters(model, path, verbose):
+def visualize_filters(model, path):
 	# add "/" to path, if it does not end on this character
 	if path[-1] != "/":
 		path += "/"
@@ -13,41 +13,38 @@ def visualize_filters(model, path, verbose):
 	# iterate over layers
 	for layer_id,layer in enumerate(model.layers):
 		# print layer id
-		if verbose:
-			print("Layer " + str(layer_id) + ":")
 
 		# ignore all layers, which are no convolutional layers
 		if layer.get_config()['name'] == "Convolution2D":
-			# iterate over filters
+			# iterate over outputs
 			for weight_matrices in layer.get_weights():
-				# print filter shape
-				if verbose:
-					print(weight_matrices.shape)
-			
 				# ignore 1 dimensional filters
 				if len(weight_matrices.shape) > 1:		
+					# get number of output maps, number of filters per input map and the x and y dimensions of the filters
 					num_outputs, num_filters, xdim, ydim = weight_matrices.shape
-					if verbose:
-						print("number of outputs:" + str(num_outputs))
-						print("number of filters:" + str(num_filters))
-						print("filter size: " + str(xdim) + "x" + str(ydim))
 
+					# calculate the size of the filter collection image and create it
 					width = num_filters * (xdim + 1) + 1
 					height = num_outputs * (ydim + 1) + 1
 					filter_collection = pil.new("L", (width, height), "white")
 				
+					# iterate over the output maps of the next layer
 					for i_id, inputs in enumerate(weight_matrices):
+						# iterate over the filters of the maps on the current layer
 						for f_id, filter in enumerate(inputs):
+							# determine position of the filter in the filter collection							
 							xpos = f_id * (xdim+1) + 1
 							ypos = i_id * (ydim+1) + 1
-							minval = np.min(filter)
-							filter += minval
-							maxval = np.max(filter)
-							filter *= 255./maxval
+
+							# rescale the grayvalues to visualize them
+							filter -= np.min(filter)
+							filter *= 255./np.max(filter)
 							im_filter = pil.fromarray(filter)
 
+							# paste the filter into the filter collection
 							filter_collection.paste(im_filter, (xpos, ypos))
 
+					# save the filter collection of layer 'id'
 					filter_collection.save(path + "weights_on_layer_" + str(layer_id) + ".png")
 
 
@@ -67,9 +64,5 @@ if args.verbose:
 	print('Loading weights from \"{0}\"'.format(args.loadpath))
 model.load_weights(args.loadpath)
 
-# print number of layers
-if args.verbose:
-	print("Number of layers: " + str(len(model.layers)))
-
 # visualize filters
-visualize_filters(model, args.savepath, args.verbose)
+visualize_filters(model, args.savepath)
