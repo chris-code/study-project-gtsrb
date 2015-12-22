9785ebb350cd210829efebb0e9a816a8b66c0ba4
chris-code <chris-code@users.noreply.github.com>
2015-12-22 22:23:19 +0100

    Made distortions optional (parameter -m, --morph)

--- src/GTSRB_train.py
+++ src/GTSRB_train.py
@@ -15,6 +15,7 @@ parser.add_argument('-e', '--epochs', help='Numper of epochs to train for [defau
 parser.add_argument('-b', '--batchsize', help='Size of the batches to be learned on [default 128]', type=int, default=128)
 parser.add_argument('-r', '--resolution', help='Resample images to AxB resolution. [default \'48x48\']', default='48x48')
 parser.add_argument('-d', '--datalimit', help='Maximum number of data points to read from PATH [if missing, read all]', type=int, default=None)
+parser.add_argument('-m', '--morph', help='Morph training data between epochs', action='store_true')
 parser.add_argument('-l', '--load-weights', help='Load weights from specified file')
 parser.add_argument('-s', '--store-weights', help='Store weights to specified file')
 parser.add_argument('-v', '--verbose', help='Set the verbosity level of keras (valid values: 0, 1, 2)', type=int, default=1)
@@ -40,11 +41,17 @@ if args.load_weights:
 	model.load_weights(args.load_weights)
 
 #~ Create distortions callback
-distcall = GTSRB_distortions.Distortions(x_train, y_train.shape[0])
+if args.morph:
+	distcall = GTSRB_distortions.Distortions(x_train, y_train.shape[0])
+	callbacks = [distcall]
+else:
+	callbacks = []
 
 #~ Train the model
 print('Training on {0} samples in batches of size {1} for {2} epochs'.format(x_train.shape[0], args.batchsize, args.epochs))
-model.fit(x_train, y_train, nb_epoch=args.epochs, callbacks=[distcall], batch_size=args.batchsize, show_accuracy=True, verbose=args.verbose)
+if args.morph:
+	print('Distortions will be applied to training data between epochs')
+model.fit(x_train, y_train, nb_epoch=args.epochs, callbacks=callbacks, batch_size=args.batchsize, show_accuracy=True, verbose=args.verbose)
 
 #~ Store weights
 if args.store_weights:
