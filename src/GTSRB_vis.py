31ea11c8e3474321768c935f88ff069a26ebb687
chris-code <chris-code@users.noreply.github.com>
2015-12-24 13:42:37 +0100

    Updated parameters in visualization
    Since -s --savepath is an optional parameter, but a value is required, it now has a default.
    Per default the images will be stored in the current directory ('./').

--- src/GTSRB_vis.py
+++ src/GTSRB_vis.py
@@ -12,46 +12,46 @@ def visualize_filters(model, path):
 
 	# iterate over layers
 	for layer_id,layer in enumerate(model.layers):
-		# print layer id
+		# ignore all layers which are not convolutional layers
+		if layer.get_config()['name'] != "Convolution2D":
+			continue
 
-		# ignore all layers, which are no convolutional layers
-		if layer.get_config()['name'] == "Convolution2D":
-			# iterate over outputs
-			for weight_matrices in layer.get_weights():
-				# ignore 1 dimensional filters
-				if len(weight_matrices.shape) > 1:		
-					# get number of output maps, number of filters per input map and the x and y dimensions of the filters
-					num_outputs, num_filters, xdim, ydim = weight_matrices.shape
+		# iterate over outputs
+		for weight_matrices in layer.get_weights():
+			# ignore 1-dimensional filters
+			if len(weight_matrices.shape) > 1:
+				# get number of output maps, number of filters per input map and the x and y dimensions of the filters
+				num_outputs, num_filters, xdim, ydim = weight_matrices.shape
 
-					# calculate the size of the filter collection image and create it
-					width = num_filters * (xdim + 1) + 1
-					height = num_outputs * (ydim + 1) + 1
-					filter_collection = pil.new("L", (width, height), "white")
-				
-					# iterate over the output maps of the next layer
-					for i_id, inputs in enumerate(weight_matrices):
-						# iterate over the filters of the maps on the current layer
-						for f_id, filter in enumerate(inputs):
-							# determine position of the filter in the filter collection							
-							xpos = f_id * (xdim+1) + 1
-							ypos = i_id * (ydim+1) + 1
+				# calculate the size of the filter collection image and create it
+				width = num_filters * (xdim + 1) + 1
+				height = num_outputs * (ydim + 1) + 1
+				filter_collection = pil.new("L", (width, height), "white")
 
-							# rescale the grayvalues to visualize them
-							filter -= np.min(filter)
-							filter *= 255./np.max(filter)
-							im_filter = pil.fromarray(filter)
+				# iterate over the output maps of the next layer
+				for i_id, inputs in enumerate(weight_matrices):
+					# iterate over the filters of the maps on the current layer
+					for f_id, filter in enumerate(inputs):
+						# determine position of the filter in the filter collection
+						xpos = f_id * (xdim+1) + 1
+						ypos = i_id * (ydim+1) + 1
 
-							# paste the filter into the filter collection
-							filter_collection.paste(im_filter, (xpos, ypos))
+						# rescale the grayvalues to visualize them
+						filter -= np.min(filter)
+						filter *= 255./np.max(filter)
+						im_filter = pil.fromarray(filter)
 
-					# save the filter collection of layer 'id'
-					filter_collection.save(path + "weights_on_layer_" + str(layer_id) + ".png")
+						# paste the filter into the filter collection
+						filter_collection.paste(im_filter, (xpos, ypos))
+
+				# save the filter collection of layer 'id'
+				filter_collection.save(path + "weights_on_layer_" + str(layer_id) + ".png")
 
 
 #~ Parse parameters
 parser = argparse.ArgumentParser()
 parser.add_argument('loadpath', help='Path to the weights which are to be loaded')
-parser.add_argument('-s', '--savepath', help='Path to save location of the visualized filters')
+parser.add_argument('-s', '--savepath', help='Path to save location of the visualized filters', default='./')
 parser.add_argument('-v', '--verbose', help='Determine whether the programm shall print information in the terminal or not', action="store_true")
 args = parser.parse_args()
 
