3036cae58c294c14bf242d82a64e1c97d8b8593d
chris-code <chris-code@users.noreply.github.com>
2016-01-06 10:55:03 +0100

    Fixed data import name conflict

---
+++ src/predict.py
@@ -0,0 +1,39 @@
+import argparse
+
+import keras.utils.np_utils as np_utils
+
+import dataset_io
+import nn
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path', help='Path to csv file that lists input images')
+parser.add_argument('-b', '--batchsize', help='Size of the batches to be learned on [default 16]', type=int, default=16)
+parser.add_argument('-r', '--resolution', help='Resample images to AxB resolution. [default \'48x48\']', default='48x48')
+parser.add_argument('-d', '--datalimit', help='Maximum number of data points to read from PATH [if missing, read all]', type=int, default=None)
+parser.add_argument('-l', '--load-weights', help='Load weights from specified .w file')
+parser.add_argument('-v', '--verbose', help='Set the verbosity level of keras (valid values: 0, 1, 2)', type=int, default=0)
+args = parser.parse_args()
+
+try:
+	sizes = args.resolution.split('x', 1)
+	resolution = (int(sizes[0]), int(sizes[1]))
+except:
+	print('Invalid resolution specification: \'{0}\''.format(args.resolution))
+	print('Resolution must be of the format \'AxB\'')
+	exit(1)
+
+#~ Load data
+x_test, y_test, num_classes = dataset_io.read_data(args.path, resolution, args.datalimit)
+y_test = np_utils.to_categorical(y_test, num_classes)
+
+input_shape = (x_test.shape[1], x_test.shape[2], x_test.shape[3])
+model, optimizer = nn.build_model(input_shape, num_classes)
+
+#~ Load weights
+print('Loading weights from {0}'.format(args.load_weights))
+model.load_weights(args.load_weights)
+
+#~ Create predictions
+print('Predicting labels for {0} samples in batches of size {1}'.format(x_test.shape[0], args.batchsize))
+pred = model.predict_classes(x_test, batch_size=args.batchsize, verbose=args.verbose)
+print(pred.shape)
