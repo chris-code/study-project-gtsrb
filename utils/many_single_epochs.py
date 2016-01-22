5905e4e20d8e9b8e2464b137f661b3fd3f0b3537
Yannick <Yannick.Schroer@rub.de>
2016-01-22 10:55:54 +0100

    Train INRIA epoch 10 to 19

--- utils/many_single_epochs.py
+++ utils/many_single_epochs.py
@@ -1,4 +1,5 @@
+import sys
 import os
 
-for i in range(4,10):
+for i in range(10,20):
 	os.system("python src/train.py layouts/inria_no_filter_train.l data/csv_list_train_inria.txt -m -l weights/inria/gtsrb_filters_0" + str(i - 1) + " -s weights/inria/gtsrb_filters_0" + str(i))
