fff95d9e3f07a6de15545c1e35585aec7cfbc5e7
Yannick <Yannick.Schroer@rub.de>
2016-01-22 11:21:18 +0100

    Train INRIA for epoch 20 to 30

--- utils/many_single_epochs.py
+++ utils/many_single_epochs.py
@@ -1,5 +1,5 @@
 import sys
 import os
 
-for i in range(10,20):
+for i in range(20,31):
 	os.system("python src/train.py layouts/inria_no_filter_train.l data/csv_list_train_inria.txt -m -l weights/inria/gtsrb_filters_0" + str(i - 1) + " -s weights/inria/gtsrb_filters_0" + str(i))
