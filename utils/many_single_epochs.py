bbf1b157a66276c5f07779d43f4b1fcc0e1fee0d
Yannick <Yannick.Schroer@rub.de>
2016-01-22 10:18:09 +0100

    First version of a script, which executes many epochs one after another and saves the weights after each epochs

---
+++ utils/many_single_epochs.py
@@ -0,0 +1,4 @@
+import os
+
+for i in range(4,10):
+	os.system("python src/train.py layouts/inria_no_filter_train.l data/csv_list_train_inria.txt -m -l weights/inria/gtsrb_filters_0" + str(i - 1) + " -s weights/inria/gtsrb_filters_0" + str(i))
